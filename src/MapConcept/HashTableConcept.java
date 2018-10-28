8766813f23b35d3df6c2afaab7c945de397af474
Satyaranjan Muduli <Satyaranjan.Muduli@in.ey.com>
2018-10-28 22:37:54 +0530

    commit java codes

--- src/MapConcept/HashTableConcept.java
+++ src/MapConcept/HashTableConcept.java
@@ -2,73 +2,75 @@ package MapConcept;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.Map.Entry;
 import java.util.Set;
 
-public class HashTableConcept {
+public class HashTableConcept
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-		
-		//it is similar to hashmap but it's synchronized i.e only one thread can access once object
-		//stores the data in key and value
-		//key -- Object --- Hashcode -- Value will be associated to the HashCode
-		//it conatins unique value
-		//it can't have any null key and null value
-		
+
+		// it is similar to hashmap but it's synchronized i.e only one thread can access
+		// once object
+		// stores the data in key and value
+		// key -- Object --- Hashcode -- Value will be associated to the HashCode
+		// it conatins unique value
+		// it can't have any null key and null value
+
 		Hashtable hs = new Hashtable();
 		hs.put(1, "Tom");
 		hs.put(2, "Test");
-		hs.put(3,"QA");
-		hs.put(3,"QA"); //it'll be ignored
-		
-		System.out.println(hs.get(3));
+		hs.put(3, "QA");
+		hs.put(3, "QA"); // it'll be ignored
+
+		System.out.println(hs);
 		System.out.println(hs.hashCode());
-		
+
 		Hashtable hs1 = new Hashtable();
-		
+
 		hs1 = (Hashtable) hs.clone();
-		
+
 		System.out.println(hs1);
-		
-		if(hs.containsValue("QA"))
-		{		
+
+		if (hs.containsValue("QA"))
+		{
 			System.out.println("exisit");
 		}
-		
-		//Pint all the values
+
+		// Pint all the values
 		Enumeration es = hs.elements();
-		
-		while(es.hasMoreElements())
+
+		while (es.hasMoreElements())
 		{
 			System.out.println(es.nextElement());
 		}
-		
-		//get all the values from HashTable from Entryset
-		
+
+		// get all the values from HashTable from Entryset
+
 		Set e = hs.entrySet();
 		System.out.println(e);
-		
-		//Check if both the Hashtable are equal or not
-		
+
+		// Check if both the Hashtable are equal or not
+
 		Hashtable hs11 = new Hashtable();
 		hs11.put(1, "Tom");
 		hs11.put(2, "Test");
-		hs11.put(3,"QA");
-		System.out.println(hs11.get(3)+"*********");
-		
-		
-		if(hs11.equals(hs))
-			System.out.println("true matching");		
-		
+		hs11.put(3, "QA");
+		System.out.println(hs11.get(3) + "*********");
+
+		if (hs11.equals(hs))
+			System.out.println("true matching");
+
 		hs11.clear();
-		
-		//generics
+
+		// generics
 		Hashtable<Integer, String> hs111 = new Hashtable<Integer, String>();
 		hs111.put(1, "Tom");
 		hs111.put(2, "Test");
-		hs111.put(3,"QA");
-		hs111.put(3,"QA");
-	}
+		hs111.put(3, "QA");
+		hs111.put(3, "QA");
 
+	}
 }
+
---
+++ src/MapConcept/LinkedHashMap.java
@@ -0,0 +1,33 @@
+package MapConcept;
+import java.util.LinkedHashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class LinkedHashMap
+{
+	public static void main(String args[])
+	{
+		//LinkedHashMap maintains insertion order
+		// HashMap Declaration
+		LinkedHashMap<Integer, String> lhmap = new LinkedHashMap<Integer, String>();
+
+		// Adding elements to LinkedHashMap
+		lhmap.put(22, "Abey");
+		lhmap.put(33, "Dawn");
+		lhmap.put(1, "Sherry");
+		lhmap.put(2, "Karon");
+		lhmap.put(100, "Jim");
+
+		// Generating a Set of entries
+		Set set = lhmap.entrySet();
+
+		// Displaying elements of LinkedHashMap
+		Iterator iterator = set.iterator();
+		while (iterator.hasNext())
+		{
+			Map.Entry me = (Map.Entry) iterator.next();
+			System.out.print("Key is: " + me.getKey() + "& Value is: " + me.getValue() + "\n");
+		}
+	}
+}
